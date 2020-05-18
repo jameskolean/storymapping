c445b68801b3002f3417a941d6705ac03671c7a3
James Kolean <jameskolean@yahoo.com>
2020-05-18 16:13:37 -0400

    Initial checkin. This displays but there are still issues.

---
+++ src/components/cards.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import StickyNote from "../components/sticky-note"
+//import { graphql } from "gatsby"
+import Typography from "@material-ui/core/Typography"
+import AddIcon from "@material-ui/icons/Add"
+import IconButton from "@material-ui/core/IconButton"
+import {
+  Container,
+  Box,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  TextField,
+} from "@material-ui/core"
+
+const useStyles = makeStyles(theme => ({
+  cell: {
+    minWidth: 175,
+    width: 175,
+    padding: 10,
+  },
+  veriticalList: {
+    padding: 0,
+    verticalAlign: "top",
+  },
+  title: {
+    fontSize: "inherit",
+  },
+  root: {
+    height: 132,
+  },
+}))
+
+const Cards = ({ cards, addStory, deleteStory }) => {
+  const classes = useStyles()
+  if (!cards) return <h4>No Cards</h4>
+  return (
+    <>
+      {cards.map(card => (
+        <TableCell key={card.id} className={classes.veriticalList}>
+          <Table>
+            <TableBody>
+              {card.stories.map(story => (
+                <TableRow key={story.id}>
+                  <TableCell className={classes.cell}>
+                    <StickyNote
+                      text={story.note}
+                      onClose={() => deleteStory(card.id, story.id)}
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+              <TableRow>
+                <TableCell
+                  className={classes.root}
+                  onClick={() => addStory(card.id)}
+                >
+                  <IconButton>
+                    <AddIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableCell>
+      ))}
+    </>
+  )
+}
+export default Cards
